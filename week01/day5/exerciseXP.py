dc6c573b93bb873909eb96a448d4924a6f42c002
GabrielK <gabriel@oppquest.com>
2026-03-27 16:31:23 +0300

    update for exercise week1 day5

---
+++ week01/day5/exerciseXP.py
@@ -0,0 +1,68 @@
+# Exercise 1: Converting Lists into Dictionaries
+# Instructions: 
+# You are given two lists. Convert them into a dictionary where the first list contains the keys and the second list contains the corresponding values.
+
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+
+new_dict = {}
+
+for k,v in zip(keys,values):
+    new_dict[k] = v
+
+print(new_dict) #{'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+
+
+# Exercise 2: Cinemax #2
+# Instructions: Write a program that calculates the total cost of movie tickets for a family based on their ages.
+    #Family members’ ages are stored in a dictionary.
+    #The ticket pricing rules are as follows:
+        #Under 3 years old: Free
+        #3 to 12 years old: $10
+        #Over 12 years old: $15
+
+
+family = {"rick": 43, 'beth': 13, 'morty': 5, 'summer': 8}
+name = input("Type name of family member: ")
+
+if name in family:
+    age = family[name]
+        
+    if age <= 3:
+        print(f"{name} pays nothing")
+    elif age <= 12:
+        print(f"{name} pays $10")
+    else:
+        print(f"{name} pays $15")
+else:
+    print("Wrong name")
+
+# Exercise 3: Zara
+# Instructions: Create and manipulate a dictionary that contains information about the Zara brand.
+
+#Create a dictionary called brand with the provided data.
+#Modify and access the dictionary as follows:
+    #Change the value of number_stores to 2.
+    #Print a sentence describing Zara’s clients using the type_of_clothes key.
+    #Add a new key country_creation with the value Spain.
+    #Check if international_competitors exists and, if so, add “Desigual” to the list.
+    #Delete the creation_date key.
+    #Print the last item in international_competitors.
+    #Print the major colors in the US.
+    #Print the number of keys in the dictionary.
+    #Print all keys of the dictionary.
+
+brand = {
+    "name": "Zara",
+    "creation_date": 1975,
+    "creator_name": "Amancio Ortega Gaona",
+    "type_of_clothes": ["men", "women", "children", "home"],
+    "international_competitors": ["Gap", "H&M", "Benetton"],
+    "number_stores": 7000,
+    "major_color": {
+    "France": "blue", 
+    "Spain": "red", 
+    "US": ["pink", "green"]}
+}
+
+
