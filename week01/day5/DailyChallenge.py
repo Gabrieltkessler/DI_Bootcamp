43215eaf7fe7bdfe62fe4d95340fde2d2d481f3f
GabrielK <gabriel@oppquest.com>
2026-03-29 08:29:58 +0300

    update

--- week01/day5/DailyChallenge.py
+++ week01/day5/DailyChallenge.py
@@ -15,3 +15,33 @@ for i, char in enumerate(user_string):
         char_str_index[char] = [i]
 
 print(char_str_index)
+
+# Challenge 2: Affordable Items
+#Goal: Create a program that prints a list of items that can be purchased with a given amount of money.
+
+items_purchase = {
+    "Water": "$1",
+    "Bread": "$3",
+    "TV": "$1,000",
+    "Fertilizer": "$20"
+}
+
+wallet = "$300"
+
+wallet = int(wallet.replace("$", "").replace(",", ""))
+
+basket = []
+
+for item, price in items_purchase.items():
+    # clean price
+    clean_price = int(price.replace("$", "").replace(",", ""))
+
+    # check if affordable
+    if clean_price <= wallet:
+        basket.append(item)
+        wallet -= clean_price
+
+if not basket:
+    print("Nothing")
+else:
+    print(sorted(basket))
