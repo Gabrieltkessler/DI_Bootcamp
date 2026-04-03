5db7f160b80b7f2c80b9f9256bac8611d5abfe8e
GabrielK <gabriel@oppquest.com>
2026-04-03 15:05:24 +0300

    update

--- week02/day1/DailyChallenge.py
+++ week02/day1/DailyChallenge.py
@@ -1,10 +1,24 @@
+#Daily challenge Gold: Solve the Matrix
+
 MATRIX_STR = '''
-7ir
-Tsi
-h%x
-i ?
-sM# 
+7ii
+Tsx
+h%?
+i #
+sM 
 $a 
 #t%'''
 
-print(MATRIX_STR)
+matrix = [list(line) for line in MATRIX_STR.splitlines() if line]
+
+decoded_message = ""
+
+for col in range(len(matrix[0])):
+    for row in matrix:
+        char = row[col]
+        if char.isalpha():
+            decoded_message += char
+        else:
+            decoded_message += " "
+
+print(decoded_message)
