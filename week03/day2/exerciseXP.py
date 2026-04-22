fb1d322dc8f4ccf7da2076892c1d4da86b594dd0
GabrielK <gabriel@oppquest.com>
2026-04-22 09:02:39 +0300

    update on exercise

--- week03/day2/exerciseXP.py
+++ week03/day2/exerciseXP.py
@@ -1,36 +1,74 @@
+#
+# class Cat:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     def walk(self):
+#         print(f"{self.name} is walking")
+#
+# class Bengal(Cat):
+#     pass
+#
+# class Chartreux(Cat):
+#     pass
+#
+# class Siamese(Cat):
+#     pass
+#
+# class Pets:
+#     def __init__(self, animals):
+#          self.animals = animals
+#
+#     def walk(self):
+#         for animal in self.animals:
+#             animal.walk()
+#
+#
+# bengal_cat = Bengal("Tony", 12)
+# chartreux_cat = Chartreux("Sasha", 9)
+# siamese_cat = Siamese("Chester", 7)
+#
+# all_cats = [bengal_cat, chartreux_cat, siamese_cat]
+#
+# sara_pets = Pets(all_cats)
+# sara_pets.walk()
 
-class Cat:
-    def __init__(self, name, age):
+# exercise 2
+
+class Dog:
+    def __init__(self, name, age, weight):
         self.name = name
         self.age = age
+        self.weight = weight
 
-    def walk(self):
-        print(f"{self.name} is walking")
+    def bark(self):
+        return f"{self.name} is barking."
 
-class Bengal(Cat):
-    pass
+    def run_speed(self):
+        return self.weight / self.age * 10
 
-class Chartreux(Cat):
-    pass
+    def fight(self, other_dog):
+        my_score = self.run_speed() * self.weight
+        other_score = other_dog.run_speed() * self.weight
 
-class Siamese(Cat):
-    pass
+        if my_score > other_score:
+            print(f"{self.name} won the fight against {other_dog.name}")
+        else:
+            print(f"{other_dog.name} won the fight against {self.name}")
 
-class Pets:
-    def __init__(self, animals):
-         self.animals = animals
+dog1 = Dog("Marley", 5, 10)
+dog2 = Dog("Jojo", 10, 90)
+dog3 = Dog("Manny", 7, 50)
 
-    def walk(self):
-        for animal in self.animals:
-            animal.walk()
+print(dog1.bark())
+print(dog2.bark())
+print(dog3.bark())
 
+print(dog1.run_speed())
+print(dog2.run_speed())
+print(dog3.run_speed())
 
-bengal_cat = Bengal("Tony", 12)
-chartreux_cat = Chartreux("Sasha", 9)
-siamese_cat = Siamese("Chester", 7)
-
-all_cats = [bengal_cat, chartreux_cat, siamese_cat]
-
-sara_pets = Pets(all_cats)
-
-sara_pets.walk()
+print(dog1.fight(dog2))
+print(dog2.fight(dog3))
+print(dog3.fight(dog1))
