edd8e3327865794417c1049a3848256dd0d9f69b
GabrielK <gabriel@oppquest.com>
2026-04-26 15:45:25 +0300

    update on daily exercise

---
+++ week03/day5/exerciseXP.py
@@ -0,0 +1,44 @@
+import random as rd
+
+def get_words_from_file(filename):
+    with open(filename, "r") as f:
+        content = f.read()
+
+    words = content.split()
+    return words
+
+words = get_words_from_file("words.txt")
+# print(words)
+
+
+def get_random_sentence(sentence_length):
+    words = get_words_from_file("words.txt")
+
+    sentence_words = []
+
+    rd.choice(words)
+
+    for _ in range(sentence_length):
+        word = rd.choice(words)
+        sentence_words.append(word)
+
+    sentence = "".join(sentence_words)
+
+    return sentence.lower()
+
+print(get_random_sentence(4))
+
+def main():
+    print("This program generates a random sentence")
+
+    input_sentence = int(input("Enter sentence length between 2 and 20: "))
+
+    if input_sentence in range(2, 21):
+        sentence = get_random_sentence(input_sentence)
+        print(f"You've chosen a length of {input_sentence} words: {sentence}")
+    else:
+        print("Please enter a number between 2 and 20")
+        raise ValueError("Invalid input range")
+
+main()
+
