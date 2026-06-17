4f4112625688e2e66dc813cf3a21dc3e01eb6af5
GabrielK <gabriel@oppquest.com>
2026-06-18 00:15:45 +0300

    Daily Challenge Update

---
+++ week08/day5/DailyChallenge.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+
