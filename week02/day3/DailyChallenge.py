6140cd8be55e692a07b2aef519652edecffa0d40
GabrielK <gabriel@oppquest.com>
2026-08-26 22:56:16 +0300

    Daily Challenge Update

--- week02/day3/DailyChallenge.py
+++ week02/day3/DailyChallenge.py
@@ -1,10 +1,19 @@
 import random
 
 list_of_numbers = [random.randint(0, 10000) for _ in range(20000)]
-
 target_number = 3728
 
+seen = set()
+found_pairs = set()
+
 for num in list_of_numbers:
-    if num + num == target_number:
-        print(f"{num} + {num} equals {target_number}")
-        break
+    complement = target_number - num
+
+    if complement in seen:
+        pair = tuple(sorted((num, complement)))
+        found_pairs.add(pair)
+
+    seen.add(num)
+
+for first, second in sorted(found_pairs):
+    print(f"{first} + {second} equals {target_number}")
